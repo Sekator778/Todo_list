eabea6b5b25b8c564182a037df8657f0665b435e
Sekator778 <sekator778@gmail.com>
2020-06-19 19:03:59 +0300

    0. Видео[6873#289802]

---
+++ src/main/java/persistence/HbmRun.java
@@ -0,0 +1,47 @@
+package persistence;
+
+import model.Role;
+import model.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import java.util.List;
+
+public class HbmRun {
+    public static void main(String[] args) {
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure().build();
+        try {
+            SessionFactory sf = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+            Role role = create(Role.of("ADMIN"), sf);
+            create(User.of("Petr Arsentev", role), sf);
+            for (User user : findAll(User.class, sf)) {
+                System.out.println(user.getName() + " " + user.getRole().getName());
+            }
+        }  catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            StandardServiceRegistryBuilder.destroy(registry);
+        }
+    }
+
+    public static <T> T create(T model, SessionFactory sf) {
+        Session session = sf.openSession();
+        session.beginTransaction();
+        session.save(model);
+        session.getTransaction().commit();
+        session.close();
+        return model;
+    }
+
+    public static <T> List<T> findAll(Class<T> cl, SessionFactory sf) {
+        Session session = sf.openSession();
+        session.beginTransaction();
+        List<T> list = session.createQuery("from " + cl.getName(), cl).list();
+        session.getTransaction().commit();
+        session.close();
+        return list;
+    }
+}
